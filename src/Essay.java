a2ca4d61631fc4a3354e2891badb3ed8bb27385c
mohamedelghaouth <44144437+mohamedelghaouth@users.noreply.github.com>
2019-08-10 11:20:24 +0100

    essay

---
+++ src/Essay.java
@@ -0,0 +1,20 @@
+
+public class Essay {
+
+	int i;
+
+	public Essay(int i) {
+		super();
+		this.i = i;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+	
+	
+}
